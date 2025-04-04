0c49cf19e97fd7ffb8a2828b3ff1ed9fba347346
CodePawsDev <isripen1991@outlook.com>
2025-04-04 10:05:08 +0700

    feat: add initial exercises

---
+++ th/ex-1.js
@@ -0,0 +1,9 @@
+const moveZeroes = function (nums) {
+  //Start coding here
+};
+
+const result1 = moveZeroes([0, 1, 0, 3, 12]);
+console.log(result1); // [1,3,12,0,0]
+
+const result2 = moveZeroes([0]);
+console.log(result2); // [0]
---
+++ th/ex-2.js
@@ -0,0 +1,12 @@
+const romanToInt = function (s) {
+  //Start coding here
+};
+
+const result1 = romanToInt("III"); // 3
+console.log(result1);
+
+const result2 = romanToInt("LVIII"); // 58
+console.log(result2);
+
+const result3 = romanToInt("MCMXCIV"); //1994
+console.log(result3);
