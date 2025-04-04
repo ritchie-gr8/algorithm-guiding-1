6b8752f86e9df430045568a4edf55686918697fc
ritchie-gr8 <rit.yotsathon@gmail.com>
2025-04-04 11:47:19 +0700

    feat: add answers

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,5 +1,22 @@
 const moveZeroes = function (nums) {
-  //Start coding here
+  // set left right pointers
+  let l = 0, r = 1
+
+  while (r < nums.length) {
+    // if found valid number swap with the l index
+    // and move l index by 1
+    if (nums[r] !== 0) {
+      let tmp = nums[l]
+      nums[l] = nums[r]
+      nums[r] = tmp
+      l++
+    }
+
+    // if r isnt valid move r index by 1
+    r++
+  }
+
+  return nums
 };
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,43 @@
 const romanToInt = function (s) {
-  //Start coding here
+  // roman value look up object
+  const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
+
+  // init sum value
+  let sum = 0
+
+  // get first value to check
+  let prevVal = roman[s[0]]
+
+  for (let i = 1; i < s.length; i++) {
+
+    // get current value (next value after prev)
+    let currVal = roman[s[i]]
+
+    // if the current value is bigger than the previous value
+    // add negative value to sum eg (CM =>  add -500 and then add 1000 so the result is 500)
+    // else add positive value to sum
+    if (currVal > prevVal) {
+      sum += -prevVal
+    } else {
+      sum += prevVal
+    }
+
+    // change previous value to current value
+    prevVal = currVal
+  }
+
+  // add last value to sum
+  sum += prevVal
+
+  return sum
 };
 
 const result1 = romanToInt("III"); // 3
